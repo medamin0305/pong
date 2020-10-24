26ea36697d361061859a2a162cf884f30a5c63d5
medamin0305 <70814531+medamin0305@users.noreply.github.com>
2020-10-24 15:07:21 +0200

    finished cp1

---
+++ Pong/Assets/MoveRacket.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour
+{
+    public float speed = 30;
+    public string axis;
+
+    //This is the speed of the racket mouvement
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {//This is our GetAxisRaw input 
+        float v = Input.GetAxisRaw(axis) * speed;
+        // Just calling the Rigidbody2D component to change its Velocity value  
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v);
+    }
+}
