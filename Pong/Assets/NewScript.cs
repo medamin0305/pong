1918d849478d7658d118d4ca882f6c8c6f2c717d
medamin0305 <70814531+medamin0305@users.noreply.github.com>
2020-10-24 17:39:12 +0200

    Fouctions

--- Pong/Assets/NewScript.cs
+++ Pong/Assets/NewScript.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewScript : MonoBehaviour
-{
+{ public int a;
+public int b;
+    int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+
+
+    int Prod(int )
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(Sum(5, 10));
+
+
+
+
+
+
 
 
     }
